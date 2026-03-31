897f53139a942baebd6ff285573f462e2ea6f7c4
tojosoa <rraharison@beachcomber.com>
2026-03-31 10:37:30 +0400

    Firt commit react-projet

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+   server: {
+    // afaka miditra avy amin'ny host na IP rehetra
+    host: true, // mba hihaino amin'ny 0.0.0.0 (tsy localhost fotsiny)
+    port: 5173,
+    allowedHosts: [
+      'utos_front.local', // hostname tianao ampiasaina amin'ny browser
+      'localhost',        // mety ilaina ihany koa
+      'frontend',         // anaran'ny container raha proxy ao Nginx
+    ],
+    hmr: {
+      //mba hifandray amin’ny Hot Module Reload (HMR) avy any amin’ny host PC
+      host: 'utos_front.local', // hostname ampiasaina ao amin'ny docker-compose / nginx
+    }
+  }
+})
